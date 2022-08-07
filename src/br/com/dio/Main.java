9ab179e7754f8b5c0954f46cbc75a5db8ad9dfea
Elisa Andrade <elisalvsan@gmail.com>
2022-08-07 18:38:52 -0300

    resolução do exercício, criação das class main e quadrilatero

---
+++ src/br/com/dio/Main.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+// Segundo exercicio - Sobrecarga
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercício Quadrilátero");
+        Quadrilatero.area(4);
+        Quadrilatero.area(6,6);
+        Quadrilatero.area(8,7,11);
+
+    }
+}
